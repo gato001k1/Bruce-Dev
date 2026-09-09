ab5d96cb95c61787ffbc77094db7f25659adfdd5
Matt Emerick-Law <matt@emericklaw.co.uk>
2026-09-09 18:39:28 +0100

    Reaper Fixes - LEDs, GPS and IR (#2868)
    * Change LED_ORDER from RGB to GRB
    
    * Rename board
    
    * Set GPS pins
    
    * Rename default pins

--- boards/reaper/pins_arduino.h
+++ boards/reaper/pins_arduino.h
@@ -103,15 +103,17 @@ static const uint8_t SCK = SPI_SCK_PIN;
 #define SDCARD_MOSI SPI_MOSI_PIN
 
 // RGB LED
-
 #define HAS_RGB_LED 1
 #define RGB_LED 45
 #define LED_TYPE WS2812B
-#define LED_ORDER RGB
+#define LED_ORDER GRB
 #define LED_TYPE_IS_RGBW 0
 #define LED_COUNT 16
-
 #define LED_COLOR_STEP 5
+
+// GPS Bus
+#define GPS_SERIAL_RX 44
+#define GPS_SERIAL_TX 43
 
 // PMIC
 // #define XPOWERS_CHIP_BQ25896
